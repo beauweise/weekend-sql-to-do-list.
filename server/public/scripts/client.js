6569e6e876c3ab2438084721b0bc18d21cbb6edf
Beau Weise <beauweise@gmail.com>
2020-10-17 16:11:07 -0500

    added get post and put in client

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,27 +1,73 @@
+const { response } = require("express");
+
 console.log('JS');
-$(document).ready(function(){
+$(document).ready(function () {
     console.log('JQ');
-    $('#submitBtn').on('click',addTask)
-    
+    $('#submitBtn').on('click', addTask)
+
 });
 
-function addTask(){
+function addTask() {
     let task = $('#task').val();
     let completed = $('#completed').val();
 
-    let tasks ={
+    let tasks = {
         task: task,
-        completed:completed
+        completed: completed
     };
     console.log(tasks);
     $.ajax({
         method: 'POST',
         url: '/tasks',
         data: tasks
-    }).then((response)=>{
+    }).then((response) => {
         console.log(response);
-        //getTasks();
-    }).catch((error)=>{
+        getTask();
+    }).catch((error) => {
         console.log(error);
     });
+}
+
+
+function getTask() {
+    console.log('in getTask');
+
+    $.ajax({
+        method: 'GET',
+        url: `/tasks`
+    }).then(function (response) {
+        console.log('get response', response);
+        for (let i = 0; i < response.length; i++)
+            $('#viewAllTasks').append(`
+            <tr>
+                <td> ${response[i].task}</td>
+                <td>${response[i].taskDone}</td>
+                <td><button data-id = '${response[i].id}' 
+                class = "${response[i].taskDone}">Done?</button></td>
+                <td><button data-id = '${response[i].id}'class = 'deleteBtn'>Delete</button></td>
+            </tr>    
+            `)
+
+    })
+}//end get tasks
+
+function deleteTask() {
+    let taskId = $(this).data('id');
+    console.log('hello from deletetask() id:', taskid);
+    $.ajax({
+        method: `DELETE`,
+        url: `/tasks/${taskid}`
+    }).then((response) => {
+        console.log(response);
+        $('#viewAllTasks').empty();
+        getTask();
+    }).catch((error) => {
+        console.log(error);
+
+    });
+}
+
+function putTask(){
+    let taskData = $(this).data('id');
+    
 }
